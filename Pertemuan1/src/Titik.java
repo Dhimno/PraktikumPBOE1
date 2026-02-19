a1a490fd13623c6492123dc7d199676907388312
Dhimno <tylerpappas91@gmail.com>
2026-02-19 11:11:54 +0700

    branch pertemuan 1

---
+++ Pertemuan1/src/Titik.java
@@ -0,0 +1,41 @@
+/* Nama File    : Titik.java
+ * Deskripsi    : berisi atribut dan method dalam class Titik
+ * Pembuat      : Dhimas Reza Nafi Wahyudi
+ * Tanggal      : 19 Februari 2026
+ */
+
+public class Titik {
+    double absis;
+    double ordinat;
+
+    Titik(){
+        absis = 0;
+        ordinat = 0;
+    }
+
+    double getAbsis(){
+        return absis;
+    }
+
+    double getOrdinat(){
+        return ordinat;
+    }
+
+    void setAbsis(double x){
+        absis = x;
+    }
+
+    void setOrdinat(double y){
+        ordinat = y;
+    }
+
+    void geser(double x, double y){
+        absis = absis + x;
+        ordinat = ordinat + x;
+    }
+
+    void printTitik(){
+        System.out.println("Titik (" + absis + "," + ordinat + ")");
+    }
+}
+
