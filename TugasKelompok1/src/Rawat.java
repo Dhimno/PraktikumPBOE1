e2737b6933d7cff44e58b24f725d0490108fc12f
Dhimno <tylerpappas91@gmail.com>
2026-03-29 11:13:38 +0700

    a

--- TugasKelompok1/src/Rawat.java
+++ TugasKelompok1/src/Rawat.java
@@ -1,3 +1,11 @@
+/* Anggota Kelompok: */
+/* Gregorius Septiano Ariadi (24060124120026) */
+/* Fadhil Yaafi Widodo (24060124140169) */
+/* Djuan Setyo Jati (24060124140163) */
+/* Dhimas Reza Nafi Wahyudi (24060124120010) */
+/* Farhan Dwiyan Akbar (24060124140137) */
+/* Kelas: E */
+
 /* File: Rawat.java */
 public class Rawat {
     protected int id;
