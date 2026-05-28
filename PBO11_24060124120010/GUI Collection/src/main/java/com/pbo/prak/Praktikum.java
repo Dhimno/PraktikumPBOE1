436a9082cca1de0c299c7f455c753d379edbc45a
Dhimno <tylerpappas91@gmail.com>
2026-05-28 12:16:52 +0700

    Add GUI collection Maven project and VSCode launch
    Introduce a new Maven project (artifactId: praktikum) under PBO11_24060124120010/GUI Collection with source files: GUI.java (Swing UI demonstrating List/Set/Map operations), GUI.form (NetBeans form metadata), and Praktikum.java (simple main). Included build artifacts (compiled classes, pom properties, and generated JAR) and maven-status metadata. Also update .vscode/launch.json to add a Java launch configuration for com.pbo.prak.GUI and extend .vscode/settings.json with Java project sourcePaths and debug/build settings.

---
+++ PBO11_24060124120010/GUI Collection/src/main/java/com/pbo/prak/Praktikum.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pbo.prak;
+
+/**
+ *
+ * @author dhimno
+ */
+public class Praktikum {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
